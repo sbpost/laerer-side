31dae87954a3276bfe33a47c1f8fcb4758588911
sbpost <sorenpost@pm.me>
2021-08-18 13:14:27 +0200

    local data sources, NEED FIX

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const NOT_OFFERED = "";
 
-const GOOGLE_DRIVE_LINK =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmqPShwjf1PloQTHS_f2PHOuGWUH80ud-zfKW5MvzVOQ_EIYG7LHWL_oI9J4luhQ/pub?output=csv";
+const GOOGLE_DRIVE_LINK = "../data/a-k.csv";
+// "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmqPShwjf1PloQTHS_f2PHOuGWUH80ud-zfKW5MvzVOQ_EIYG7LHWL_oI9J4luhQ/pub?output=csv";
 
 // const MATERIALE_LINK = "../data/materiale-kortlægning2.csv";
 const MATERIALE_LINK = "../data/m-k.csv";
