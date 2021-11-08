5cb987416428511baebd9cdd6c8c342df1e6b39e
spbost <sorenpost@pm.me>
2021-11-08 16:26:40 +0100

    Removed link-highlights from inner nodes

--- src/dendogram.js
+++ src/dendogram.js
@@ -6,7 +6,7 @@ function depth_scale(depth) {
 
 function project(x, y, depth) {
   var angle = ((x - 90) / 180) * Math.PI,
-    radius = y - depth_scale(depth);
+      radius = y - depth_scale(depth);
 
   // var angle = (x - 90) / 180 * Math.PI, radius = y;
   return [radius * Math.cos(angle), radius * Math.sin(angle)];
@@ -22,56 +22,56 @@ let height = 1200;
 let radius = 390;
 let radius_scaling = 1;
 var svg = d3
-  .select("#tree-chart")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .append("g")
-  .attr(
-    "transform",
-    "translate(" + (width / 2 - 15) + "," + (height / 2 + 25) + ")"
-  );
+    .select("#tree-chart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr(
+      "transform",
+      "translate(" + (width / 2 - 15) + "," + (height / 2 + 25) + ")"
+    );
 
 // Add logos
 // TODO: Grab parent node position and substitute for the 0.
 let center_logo_width = radius * 2 - 220;
 let center_logo_height = radius * 2 - 220;
 let centerlogo = svg
-  .append("svg:image")
-  .attr("x", 0 - center_logo_width / 2)
-  .attr("y", 0 - center_logo_height / 2)
-  .attr("width", center_logo_width)
-  .attr("height", center_logo_height)
-  .attr("xlink:href", "./imgs/DGR_logo_cirkel_negativ_cropped.png");
+    .append("svg:image")
+    .attr("x", 0 - center_logo_width / 2)
+    .attr("y", 0 - center_logo_height / 2)
+    .attr("width", center_logo_width)
+    .attr("height", center_logo_height)
+    .attr("xlink:href", "./imgs/DGR_logo_cirkel_negativ_cropped.png");
 
 let cornerlogo = svg
-  .append("svg:image")
-  .attr("x", 400)
-  .attr("y", 400)
-  .attr("width", 2214 / 20)
-  .attr("height", 1600 / 20)
-  .attr("xlink:href", "./imgs/DGR_logo.png");
+    .append("svg:image")
+    .attr("x", 400)
+    .attr("y", 400)
+    .attr("width", 2214 / 20)
+    .attr("height", 1600 / 20)
+    .attr("xlink:href", "./imgs/DGR_logo.png");
 
 // Create the cluster layout:
 let clusterMap = d3.cluster().size([360, radius]);
 
 let stratify = d3
-  .stratify()
-  .id(function (d) {
-    return d.name;
-  })
-  .parentId(function (d) {
-    return d.parent;
-  });
+    .stratify()
+    .id(function (d) {
+      return d.name;
+    })
+    .parentId(function (d) {
+      return d.parent;
+    });
 
 let linksGenerator = d3
-  .linkRadial()
-  .angle(function (d) {
-    return (d.x / 180) * Math.PI;
-  })
-  .radius(function (d) {
-    return d.y - depth_scale(d.depth);
-  });
+    .linkRadial()
+    .angle(function (d) {
+      return (d.x / 180) * Math.PI;
+    })
+    .radius(function (d) {
+      return d.y - depth_scale(d.depth);
+    });
 
 // Dum link (entry link, i.e. start of transition)
 let dumLink = d3.linkRadial().angle(0).radius(0);
@@ -125,24 +125,24 @@ function updateDendogramFigure(data) {
   let u_node = svg.selectAll("g").data(nodes.descendants());
 
   let nodeEnter = u_node
-    .enter()
-    .append("g")
-    .attr("class", function (d) {
-      let assign_class =
-        "node" + (d.children ? " node--internal" : " node--leaf");
-      if (d.depth === 2) {
-        assign_class =
-          assign_class +
-          (d.data.link === "" ? " node--unlinked" : " node--linked");
-      }
-      return assign_class;
-    });
+      .enter()
+      .append("g")
+      .attr("class", function (d) {
+        let assign_class =
+            "node" + (d.children ? " node--internal" : " node--leaf");
+        if (d.depth === 2) {
+          assign_class =
+            assign_class +
+            (d.data.link === "" ? " node--unlinked" : " node--linked");
+        }
+        return assign_class;
+      });
 
   // Attach a piece of text to each "g" (the container for each node)
   svg
     .selectAll("g")
     .append("text")
-    //nodeEnter.append("text")
+  //nodeEnter.append("text")
     .attr("dy", ".31em")
     .attr("x", function (d) {
       return d.x < 180 === !d.children ? 10 : -10;
@@ -154,7 +154,7 @@ function updateDendogramFigure(data) {
     .attr("transform", function (d) {
       return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")";
     })
-    // .text(function(d) { return d.data.name == active_layer ? "" : d.data.name });
+  // .text(function(d) { return d.data.name == active_layer ? "" : d.data.name });
     .text(function (d) {
       return d.depth === 0 ? "" : d.data.name;
     })
@@ -173,7 +173,7 @@ function updateDendogramFigure(data) {
     .style("opacity", 0.0)
     .append("circle")
     .attr("r", 6)
- //   .attr("fill", "#999")
+  //   .attr("fill", "#999")
     .on("mouseover", mouseOverHandler)
     .on("mouseout", mouseOutHandler)
     .on("click", function (event, d) {
@@ -181,7 +181,7 @@ function updateDendogramFigure(data) {
         window.open(d.data.link);
       }
     })
-    // If all themes are chosen, make nodes gray. Otherwise, color by theme.
+  // If all themes are chosen, make nodes gray. Otherwise, color by theme.
     .attr("fill", (d) => {
       return current_selection.theme.length == THEMES.length
         ? "#999"
@@ -229,11 +229,14 @@ function mouseOverHandler(event, d) {
   // if the node does not have a link, make different color
   let g = d3.select(event.target.parentElement);
   g.classed("highlighted", true);
-  if (g.classed("node--linked")) {
-    g.classed("with-link", true);
-    g.style("cursor", "pointer");
-  } else {
-    g.classed("without-link", true);
+  // Only highlight nodes with/without link if it is a leaf node
+  if (g.classed("node--leaf")) {
+    if (g.classed("node--linked")) {
+      g.classed("with-link", true);
+      g.style("cursor", "pointer");
+    } else {
+      g.classed("without-link", true);
+    }
   }
 }
 
